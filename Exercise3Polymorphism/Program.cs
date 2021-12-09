1742dd43e281ad07fdf64245f401cfd0edc626aa
Radha Manickam <radhaarul@gmail.com>
2021-12-09 14:53:15 +0100

    Add project files.

---
+++ Exercise3Polymorphism/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise3Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<UserError> UE = new List<UserError>();
+            UserError NError, TError,NIError,NZNError,NVSError;
+            NError = new NumericInputError();
+            TError = new TextInputError();
+            NIError = new NullInputError();
+            NZNError = new NegativeZeroNumericError();
+            NVSError= new NumericValueSeperatorError();
+            UE.Add(NError);
+            UE.Add(TError);
+            UE.Add(NIError);
+            UE.Add(NZNError);
+            UE.Add(NVSError);
+            foreach (var e in UE)
+                Console.WriteLine(e.UEMessage());
+
+
+        }
+    }
+}
---
+++ Exercise3Polymorphism/UserError.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3Polymorphism
+{
+    public abstract class UserError
+    {
+        public abstract string UEMessage();
+    }
+    public class NumericInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return " You tried to use a numeric input in a text only field. This fired an error!";
+        }
+    }
+    public class TextInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "You tried to use a text input in a numeric only field. This fired an error! ";
+        }
+    }
+
+    public class NullInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "Field should not be Null. This fired an error!";
+        }
+    }
+    public class NegativeZeroNumericError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "You tried to use Negative or Zero input. This fired an error! ";
+        }
+    }
+    public class NumericValueSeperatorError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "Yor tried to give wrong format of input. This fired an error! ";
+        }
+    }
+}
